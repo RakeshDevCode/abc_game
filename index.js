b03e4c08de40172f5708e0182f32c8cffa12ad25
RakeshDevCode <146819010+RakeshDevCode@users.noreply.github.com>
2023-10-18 17:59:53 +0530

    Update index.js
    short code from previous code
    new updated feature click random text generator and press any key  to generate random text

--- index.js
+++ index.js
@@ -1,34 +1,12 @@
+$("h1").on("click",function(){
+{function randomtext() {
+    var random1="ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz0123456789";
+    var num1=Math.floor(Math.random()*62)+1;
+    return random1[num1];
+    }
+$("h1").text(randomtext()); }
+});
+
 $(document).keydown(function(event){
-   $("h1").text(event.key);
-});
-
-$(document).keydown(function(length) {
-   var result = '';
-   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-   const charactersLength = characters.length;
-   var counter = 0;
-   while (counter < length) {
-     result += characters.charAt(Math.floor(Math.random() * charactersLength));
-     counter += 1;
-   }
-   $("h1").text(result);
-});
-
-
-function randomString() {  
-   //define a variable consisting alphabets in small and capital letter  
-var characters = "ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";  
-     
-   //specify the length for the new string  
-var lenString = 1;  
-var randomstring = '';  
-
-   //loop to select a new character in each iteration  
-for (var i=0; i<lenString; i++) {  
-var rnum = Math.floor(Math.random() * characters.length);  
-randomstring += characters.substring(rnum, rnum+1);  
-}  
-
-    //display the generated string   
-document.getElementById("randomfield").innerHTML = randomstring;  
-}  
+    $("h2").text(event.key);
+ });
